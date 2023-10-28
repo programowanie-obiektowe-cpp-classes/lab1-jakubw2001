d71d3307c71bc3a42f71adeeb97bd0a08c9228e3
Jakub Wiecha <147273231+jakubw2001@users.noreply.github.com>
2023-10-28 19:32:19 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,31 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D(){}
+    Wektor2D(double x, double y){
+        X = x;
+        Y = y;
+    }
+    Wektor2D operator+(Wektor2D const& obj){
+        Wektor2D res;
+        res.X = X + obj.X;
+        res.Y = Y + obj.Y;
+        return res;
+    }
+    double operator*(Wektor2D const& obj){
+        Wektor2D res;
+        res.X = X * obj.X;
+        res.Y = Y * obj.Y;
+        return res.X + res.Y;
+    }
+    void setX(double x) {X = x;}
+    void setY(double y) {Y = y;}
+    double getX() {return X;}
+    double getY() {return Y;}
+    Wektor2D sum(Wektor2D v1, Wektor2D v2)
+    {
+        return Wektor2D(v1.getX()+v2.getX(), v2.getY()+v2.getY());
+    }
+private:
+    double X, Y;
 };
